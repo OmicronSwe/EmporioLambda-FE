58b8e951a31b75b746b3f819d2fd2514f71175c0
MatthewBalzan <matthewb487@gmail.com>
2021-03-14 19:45:30 +0100

    UI order and category sections

--- components/Dashboard/CategorySection.tsx
+++ components/Dashboard/CategorySection.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Accordion, Button, Card, CardColumns, Col, Form, Row } from "react-bootstrap";
 
 class CategorySection extends React.Component<{}> {
   constructor(props) {
@@ -7,7 +8,82 @@ class CategorySection extends React.Component<{}> {
   }
 
   render() {
-    return <h1>Category Section</h1>;
+    return (
+      <>
+        <h1>Category Section</h1>
+        <Accordion style={{ width: "50%" }}>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} eventKey="0">
+                Add new category
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <Form>
+                  <Form.Group as={Row}>
+                    <Form.Label column sm="4">
+                      Category name
+                    </Form.Label>
+                    <Col sm="8">
+                      {" "}
+                      <Form.Control placeholder="Name" />
+                    </Col>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+        <CardColumns>
+          <Card>
+            <Card.Body>
+              <Card.Title>Name</Card.Title>
+              <Button variant="danger">Remove</Button>
+              <Button variant="warning">Modify</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Name</Card.Title>
+              <Button variant="danger">Remove</Button>
+              <Button variant="warning">Modify</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Name</Card.Title>
+              <Button variant="danger">Remove</Button>
+              <Button variant="warning">Modify</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Name</Card.Title>
+              <Button variant="danger">Remove</Button>
+              <Button variant="warning">Modify</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Name</Card.Title>
+              <Button variant="danger">Remove</Button>
+              <Button variant="warning">Modify</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Name</Card.Title>
+              <Button variant="danger">Remove</Button>
+              <Button variant="warning">Modify</Button>
+            </Card.Body>
+          </Card>
+        </CardColumns>
+      </>
+    );
   }
 }
 
--- components/Dashboard/OrderSection.tsx
+++ components/Dashboard/OrderSection.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Table } from "react-bootstrap";
 
 class OrderSection extends React.Component<{}> {
   constructor(props) {
@@ -7,7 +8,42 @@ class OrderSection extends React.Component<{}> {
   }
 
   render() {
-    return <h1>Order Section</h1>;
+    return (
+      <>
+        <h1>Orders Section</h1>
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Client</th>
+              <th>Date</th>
+              <th>Total cost</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Matthew</td>
+              <td>2021-01-01</td>
+              <td>32.99$</td>
+              <td>
+                <Button>Details</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Gabriel</td>
+              <td>2021-01-02</td>
+              <td>23.99$</td>
+              <td>
+                <Button>Details</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </>
+    );
   }
 }
 
