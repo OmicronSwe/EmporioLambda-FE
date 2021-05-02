6b2db5cbbea1c52e1060760845b9ead64379c7e0
killbizz <45452791+killbizz@users.noreply.github.com>
2021-05-02 16:01:33 +0200

    fix lint

--- components/Dashboard/ProductList.tsx
+++ components/Dashboard/ProductList.tsx
@@ -9,63 +9,63 @@ interface ProductListProps {
 }
 
 const ProductList = ({ products, removeProduct }: ProductListProps) => {
-    return (
-      <>
-        <Table>
-          <thead className="thead-light">
-            <tr>
-              <th>ID</th>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Category</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products ? (
-              products.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>
-                    <img
-                      src={item.imageUrl ? item.imageUrl : ""}
-                      className="img-thumbnail"
-                      alt={item.name}
-                      width={100}
-                    />
-                  </td>
-                  <td>{item.name}</td>
-                  <td>{item.description}</td>
-                  {"category" in item ? <td>{item.category}</td> : <td />}
-                  {"price" in item ? <td>{`${item.price}€`}</td> : <td />}
-                  <td>
-                    <Button
-                      variant="warning"
-                      onClick={() => Router.push(`/dashboard/modify/${item.id}`)}
-                    >
-                      Modify
-                    </Button>
-                  </td>
-                  <td>
-                    <Button
-                      variant="danger"
-                      onClick={() => {
-                        removeProduct(item.id);
-                      }}
-                    >
-                      Remove
-                    </Button>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <p>No product found</p>
-            )}
-          </tbody>
-        </Table>
-      </>
-    );
-  }
+  return (
+    <>
+      <Table>
+        <thead className="thead-light">
+          <tr>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products ? (
+            products.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>
+                  <img
+                    src={item.imageUrl ? item.imageUrl : ""}
+                    className="img-thumbnail"
+                    alt={item.name}
+                    width={100}
+                  />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                {"category" in item ? <td>{item.category}</td> : <td />}
+                {"price" in item ? <td>{`${item.price}€`}</td> : <td />}
+                <td>
+                  <Button
+                    variant="warning"
+                    onClick={() => Router.push(`/dashboard/modify/${item.id}`)}
+                  >
+                    Modify
+                  </Button>
+                </td>
+                <td>
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      removeProduct(item.id);
+                    }}
+                  >
+                    Remove
+                  </Button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <p>No product found</p>
+          )}
+        </tbody>
+      </Table>
+    </>
+  );
+};
 
 export default ProductList;
