fce34db18d4ba92e3b23d8248d3d865eec01e1e0
killbizz <45452791+killbizz@users.noreply.github.com>
2021-05-10 12:45:27 +0200

    dashboard refactoring

--- components/Dashboard/CategoryList.tsx
+++ components/Dashboard/CategoryList.tsx
@@ -1,35 +1,32 @@
 import React from "react";
 import { Button, CardColumns, Card } from "react-bootstrap";
 
-class CategoryList extends React.Component<{ categories: string[]; removeCategory }> {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+interface CategoryListProps {
+  categories: string[];
+  removeCategory;
+}
 
-  render() {
-    const { categories, removeCategory } = this.props;
-    return (
-      <>
-        <CardColumns>
-          {categories ? (
-            categories.map((item) => (
-              <Card key={item}>
-                <Card.Body>
-                  <Card.Title>{item}</Card.Title>
-                  <Button variant="danger" onClick={() => removeCategory(item)}>
-                    Remove
-                  </Button>
-                </Card.Body>
-              </Card>
-            ))
-          ) : (
-            <p>No category found</p>
-          )}
-        </CardColumns>
-      </>
-    );
-  }
+const CategoryList = ({ categories, removeCategory }: CategoryListProps) => {
+  return (
+    <>
+      <CardColumns>
+        {categories ? (
+          categories.map((item) => (
+            <Card key={item}>
+              <Card.Body>
+                <Card.Title>{item}</Card.Title>
+                <Button variant="danger" onClick={() => removeCategory(item)}>
+                  Remove
+                </Button>
+              </Card.Body>
+            </Card>
+          ))
+        ) : (
+          <p>No category found</p>
+        )}
+      </CardColumns>
+    </>
+  );
 }
 
 export default CategoryList;
--- components/Dashboard/OrderList.tsx
+++ components/Dashboard/OrderList.tsx
@@ -2,46 +2,42 @@ import React from "react";
 import { Table, Button } from "react-bootstrap";
 import Order from "../../src/types/Order";
 
-class ProductList extends React.Component<{ orders: Order[] }> {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { orders } = this.props;
-    return (
-      <>
-        {orders ? (
-          <Table>
-            <thead className="thead-light">
-              <tr>
-                <th>ID ordine</th>
-                <th>Client</th>
-                <th>Total cost</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((item) => (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.email}</td>
-                  <td>{`${item.totalPrice.toFixed(2)}€`}</td>
-                  <td>{item.date}</td>
-                  <td>
-                    <Button variant="primary">Details</Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        ) : (
-          <h3>No orders found</h3>
-        )}
-      </>
-    );
-  }
+interface OrderListProps {
+  orders: Order[];
 }
 
-export default ProductList;
+const OrderList = ({ orders }: OrderListProps) => {
+  return (
+    <>
+      {orders ? (
+        <Table>
+          <thead className="thead-light">
+            <tr>
+              <th>ID ordine</th>
+              <th>Client</th>
+              <th>Total cost</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{`${item.totalPrice.toFixed(2)}€`}</td>
+                <td>{`${new Date(item.date).toLocaleDateString()}`}</td>
+                <td>
+                  <Button variant="primary">Details</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <h3>No orders found</h3>
+      )}
+    </>
+  );
+}
+
+export default OrderList;
